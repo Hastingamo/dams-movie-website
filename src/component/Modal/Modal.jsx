bb7794ac7bef98dac1012dfdaeea637369635ade
Hastingsamo.com <hezekiahhastings14@gmail.com>
2025-02-09 17:40:17 +0100

    theModal

--- src/component/Modal/Modal.jsx
+++ src/component/Modal/Modal.jsx
@@ -1,15 +1,13 @@
-function Modal({title}) {
+function Modal({ title }) {
   return (
     <>
-      <img
-        className="w-5 h-5"
-        src="images/dots.png"
-        alt=""
-      />
+      <img className="w-5 h-5" src="images/dots.png" alt="" />
       <div>
-        <div className="w-">
-            <img src="images/" alt=""/> 
-            <h1>{title}</h1>  
+        <div className="w-48 h-48 bg-white">
+          <div className="flex flex-row mt-6">
+            <img src="images/" alt="" />
+            <h1>{title}</h1>
+          </div>
         </div>
       </div>
     </>
