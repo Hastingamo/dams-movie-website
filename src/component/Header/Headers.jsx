4e2f209ce898b7e23307eaa84e4b58ee2e3df8f5
Hastingsamo.com <hezekiahhastings14@gmail.com>
2025-03-27 01:33:38 +0100

    they

--- src/component/Header/Headers.jsx
+++ src/component/Header/Headers.jsx
@@ -4,21 +4,7 @@ import Headerss from "./Headerss";
 function Headers() {
   return (
     <>
-      <header className="sm:sticky">
-        <nav className="hidden  w-[14.8rem] h-screen bg-[#515050] sm:flex flex-col text-white text-4xl font-normal italic pl-11 pt-4 gap-10">
-          {/* <Link to="Home">Home</Link> */}
-          <Link to="Series">Series</Link>
-          <Link to="Movies">Movies</Link>
-          <Link to="Cartoon">Cartoon</Link>
-          <Link to="Login">Login</Link>
-          <Link to="Profile">Profile</Link>
-          <Link to="AddToList">AddToList</Link>
-
-
-
-        
-        </nav>
-      </header>
+      
       <header className=" h-10 w-full bg-[#d3baba88] flex flex-row xs:w-[27rem] xm:w-[33.6rem] sm:hidden">
         {/* <h1 className="flex justify-center items-center">Dams</h1> */}
         <h1 className="ml-11">Dams</h1>
