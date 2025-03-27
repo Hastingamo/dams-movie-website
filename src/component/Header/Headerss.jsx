896997ed1b1a9a8ab78c2e757e0e82ffcaf235ac
Hastingsamo.com <hezekiahhastings14@gmail.com>
2025-03-27 23:18:14 +0100

    they

--- src/component/Header/Headerss.jsx
+++ src/component/Header/Headerss.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 function Headerss() {
   return (
     <>
-      <header className=" h-10 w-full bg-[#d3baba88] flex flex-row xs:w-[27rem] xm:w-[33.6rem] sm:hidden">
+      <header className=" h-10 w-full bg-[#d3baba88] flex flex-row  sm:hidden">
         {/* <h1 className="flex justify-center items-center">Dams</h1> */}
         <h1 className="ml-11">Dams</h1>
         <div className="ml-14 gap-8 flex flex-row">
