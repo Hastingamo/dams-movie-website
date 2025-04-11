950e680604ba4cd4a0ce8708fc22bed08f7091f4
Hastingsamo.com <hezekiahhastings14@gmail.com>
2025-04-11 23:14:44 +0100

    they

--- src/component/Header/Headers.jsx
+++ src/component/Header/Headers.jsx
@@ -7,14 +7,14 @@ function Headers() {
       <header className="hidden xm:hidden md:h-screen xm:w-[20rem] md:flex md:flex-col bg-[#515050] ">
         <div className="ml-14 gap-8 flex flex-col md:text-2xl text-white">
         <h1 className="mt-4">Dams</h1>
-        <Link to="Home">Home</Link>
-        <Link to="Movies">Movies</Link>
-        <Link to="Series">Series</Link>
-        <Link to="AddToList">AddToList</Link>
+        <Link className="mr-3 hover:bg-slate-400 hover:w-auto active:bg-slate-700" to="Home">Home</Link>
+        <Link className="mr-3 hover:bg-slate-400 hover:w-auto active:bg-slate-700" to="Movies">Movies</Link>
+        <Link className="mr-3 hover:bg-slate-400 hover:w-auto active:bg-slate-700" to="Series">Series</Link>
+        <Link className="mr-3 hover:bg-slate-400 hover:w-auto active:bg-slate-700" to="AddToList">AddToList</Link>
         {/* <Link to="MoviesDetails">MoviesDetails</Link> */}
-        <Link to="Profile">Profile</Link>
-          <Link to="Login">Login</Link>
-          <Link to="SignUp">SignUp</Link>
+        <Link className="mr-3 hover:bg-slate-400 hover:w-auto active:bg-slate-700" to="Profile">Profile</Link>
+          <Link className="mr-3 hover:bg-slate-400 hover:w-auto active:bg-slate-700" to="Login">Login</Link>
+          <Link className="mr-3 hover:bg-slate-400 hover:w-auto active:bg-slate-700" to="SignUp">SignUp</Link>
         </div>
       </header>
 
