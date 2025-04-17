cc91d3c5a7697ff2bec1d55d9b8d5c3f2b04b7be
Hastingsamo.com <hezekiahhastings14@gmail.com>
2025-04-17 12:33:12 +0100

    Fix indentation for Footers component in Movie detail view

--- src/pag/MoviesDetail/movie.jsx
+++ src/pag/MoviesDetail/movie.jsx
@@ -49,8 +49,9 @@ function Movie() {
                   </Link>
                 )
             )}
-            <Footers />
           </div>
+          <Footers />
+
         </div>
       )}
     </>
