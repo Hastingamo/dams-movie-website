b4bd1296b643bcbf6be6024e184c9738daf49d85
Hastingsamo.com <hezekiahhastings14@gmail.com>
2025-04-18 19:59:20 +0100

    Add Home route to Routes in App component

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,8 @@ function App() {
         <div className="flex flex-col">
           <Routes>
             <Route path="" element={<Home />}></Route>
+            <Route path="Home" element={<Home />}></Route>
+
             <Route path="Rgister" element={<Rgister />}></Route>
             <Route path="Movies" element={<Movies />}></Route>
             <Route path="Series" element={<Series />}></Route>
